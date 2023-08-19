a680e91ae6e688683814e09de79830e294e4076c
antiless <auv1107@gmail.com>
2023-08-19 19:16:29 +0800

    add rest files
    Signed-off-by: antiless <auv1107@gmail.com>

---
+++ src/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    title: 'CleanClip',
+    description: 'Mac 上完美的白色家电，专注于沉浸式体验的极简剪贴板应用',
+    themeConfig: {
+        nav: [
+          { text: '演示视频', link: 'https://www.bilibili.com/video/BV1594y1r7nF/?vd_source=49d41f6da6ceddbaeb919efc1f0d9f00' },
+          { text: '限时特惠，终身会员五折优惠！', link: 'https://p.ipic.vip/m9duho.JPG', target:'_blank' },
+          {
+            text: '联系我们',
+            ariaLabel: '反馈菜单',
+            items: [
+              { text: 'Twitter', link: 'https://twitter.com/s1ntone', target:'_blank' },
+              { text: 'Weibo', link: 'https://weibo.com/u/2293237681', target:'_blank' },
+              { text: '邮箱 📮', link: 'mailto:cleanclip.gokoding.com', target:'_blank'},
+              { text: '微信群', link: 'https://p.ipic.vip/m9duho.JPG', target:'_blank'}
+            ]
+          }
+        ],
+        search: false
+      }    
+}
