8265fb75d5559542e944eb3992ed134b0af38996
antiless <auv1107@gmail.com>
2023-08-22 22:46:13 +0800

    添加 appcast
    Signed-off-by: antiless <auv1107@gmail.com>

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -17,6 +17,10 @@ module.exports = {
             ]
           }
         ],
-        search: false
+        search: false,
+        sidebar: [
+          '/',
+          '/text'
+        ]
       }    
 }
