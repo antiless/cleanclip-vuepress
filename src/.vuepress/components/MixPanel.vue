b7778c06c71ff4b2a7dc419179a02e536dfc1d21
auv1107 <auv1107@gmail.com>
2024-01-04 14:29:32 +0800

    add missing file
    Signed-off-by: auv1107 <auv1107@gmail.com>

---
+++ src/.vuepress/components/MixPanel.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+<script setup>
+var mixpanel = require('mixpanel-browser');
+const TOKEN = "b5cde503ff2a03a5d34165c99c41b367"
+
+mixpanel.init(TOKEN, { debug: false, track_pageview: true, persistence: 'localStorage', api_host: "https://mixpanel-proxy.cleanclip.cc" });
+console.log("mixpanel inited")
+</script>
+<style scoped>
+</style>
