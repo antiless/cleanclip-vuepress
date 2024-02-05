0ac50d04a22d1c4629f9a8ddf36a5cd0d538adc8
auv1107 <auv1107@gmail.com>
2024-02-06 01:25:12 +0800

    压缩 tailwindcss
    Signed-off-by: auv1107 <auv1107@gmail.com>

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -19,7 +19,7 @@ module.exports = {
     ],
     theme: 'antdocs',
     head: [
-      ['link', { rel: 'stylesheet', href: '/css/output.css' }],
+      ['link', { rel: 'stylesheet', href: '/css/tailwindcss.min.css' }],
       ["link", { rel: "icon", type: "image/ico", href: "/images/favicon.ico" }],
       ["link", { rel: "shortcut icon", type: "image/x-icon", href: "/images/favicon.ico" }],
       ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/images/favicon-32x32.png' }],
