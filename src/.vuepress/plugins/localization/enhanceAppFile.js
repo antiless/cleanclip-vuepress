c78566afd863689c7b23df094d9c2af8c109e510
auv1107 <auv1107@gmail.com>
2024-03-13 16:38:34 +0800

    修改文档样式
    Signed-off-by: auv1107 <auv1107@gmail.com>

--- src/.vuepress/plugins/localization/enhanceAppFile.js
+++ src/.vuepress/plugins/localization/enhanceAppFile.js
@@ -4,9 +4,7 @@ const strings = require("../../config/strings/index")
 export default ({ Vue }) => {
   // eslint-disable-next-line vue/match-component-file-name
   // 注册全局方法
-  console.log('注册一个全局方法');
   Vue.prototype.$t = function () {
-    console.log('这是一个全局方法', this.$lang);
     return strings.strings(this.$lang)
   };
 }
