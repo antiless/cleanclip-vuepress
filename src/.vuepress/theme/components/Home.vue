9b01c34c7f4743c23b8d04a07399af0a3fa437ad
auv1107 <auv1107@gmail.com>
2024-03-24 19:28:47 +0800

    添加 Product Hunt 支持
    Signed-off-by: auv1107 <auv1107@gmail.com>

--- src/.vuepress/theme/components/Home.vue
+++ src/.vuepress/theme/components/Home.vue
@@ -41,6 +41,12 @@
           </a-space>
         </div>
 
+        <div class="flex flex-col justify-center mt-10">
+          Support us on Product Hunt:
+          <div class="flex justify-center mt-2">
+            <a href="https://www.producthunt.com/posts/cleanclip?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-cleanclip" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=417648&theme=light" alt="CleanClip - The&#0032;easiest&#0032;way&#0032;to&#0032;use&#0032;clipboard&#0032;history&#0032;like&#0032;a&#0032;pro | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
+          </div>
+        </div>
       </header>
 
       <div v-if="data.features && data.features.length" class="features">
