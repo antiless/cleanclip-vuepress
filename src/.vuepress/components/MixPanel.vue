6fc5045b00ec01b995716687d66dd5a0739f880e
sintone <auv1107@gmail.com>
2024-09-16 11:37:20 +0800

    删除 mixpanel 依赖
    Signed-off-by: sintone <auv1107@gmail.com>

--- src/.vuepress/components/MixPanel.vue
+++ src/.vuepress/components/MixPanel.vue
@@ -2,12 +2,12 @@
   <div></div>
 </template>
 <script setup>
-var mixpanel = require('mixpanel-browser');
-const TOKEN = "b5cde503ff2a03a5d34165c99c41b367"
+// var mixpanel = require('mixpanel-browser');
+// const TOKEN = "b5cde503ff2a03a5d34165c99c41b367"
 
 // mixpanel.init(TOKEN, { debug: false, track_pageview: true, persistence: 'localStorage', api_host: "https://mixpanel-proxy.cleanclip.cc" });
-mixpanel.init(TOKEN, { debug: false, track_pageview: true, persistence: 'localStorage' });
-console.log("mixpanel inited")
+// mixpanel.init(TOKEN, { debug: false, track_pageview: true, persistence: 'localStorage' });
+// console.log("mixpanel inited")
 </script>
 <style scoped>
 </style>
--- src/index.md
+++ src/index.md
@@ -145,4 +145,3 @@ autometa:
 </div>
 
 <NewFooter/>
-<MixPanel/>
