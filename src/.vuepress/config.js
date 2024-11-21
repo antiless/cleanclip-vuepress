e14a21a0b051e65ce25b4fd3bb23fd49e77aa031
sintone <auv1107@gmail.com>
2024-11-21 11:50:47 +0800

    add 2.3.2
    Signed-off-by: sintone <auv1107@gmail.com>

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -62,8 +62,8 @@ module.exports = {
   themeConfig: {
     title: "CleanClip",
     logo: '/images/logo.webp',
-    latestDownloadUrl: 'https://cleanclip.cc/releases/download/v2.3.1/CleanClip.dmg',
-    latestVersion: 'v2.3.1',
+    latestDownloadUrl: 'https://cleanclip.cc/releases/download/v2.3.2/CleanClip.dmg',
+    latestVersion: 'v2.3.2',
     freeTrailUrl: 'https://clip-purchase.macaify.com/freetrail',
     purchaseUrl: 'https://clip-purchase.macaify.com',
     smoothScroll: true,
